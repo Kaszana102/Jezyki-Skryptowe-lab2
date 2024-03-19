bae15f176aed5a03a0d6d96dfcc5c5bb0498c137
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2024-03-19 07:40:11 +0100

    prawie koniec

---
+++ cpp/cpp/cpp.c
@@ -0,0 +1,368 @@
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include "structmember.h"
+
+
+typedef struct {
+    PyObject_HEAD    
+    int graph_vertices;
+    int graph_adjacencyMatrix[16];
+} AdjacencyMatrix;
+
+void PrintMatrix(int bytes[16]) {
+    for (int i = 0; i < 16; i++) {
+        printf("%d\n", bytes[i]);
+    }
+}
+void AddEdge(AdjacencyMatrix* self, int i, int j) {
+    if (i < j) {
+        int temp = i;
+        i = j;
+        j = temp;
+    }
+
+    self->graph_adjacencyMatrix[i] |= 1 << j;
+}
+static void
+Custom_dealloc(AdjacencyMatrix* self)
+{    
+    Py_TYPE(self)->tp_free((PyObject*)self);
+}
+
+static PyObject*
+Custom_new(PyTypeObject* type, PyObject* args, PyObject* kwds)
+{
+
+    AdjacencyMatrix* self;
+    self = (AdjacencyMatrix*)type->tp_alloc(type, 0);
+    if (self != NULL) {
+        self->graph_vertices = 0;
+        for (int i = 0; i < 16; i++) {
+            self->graph_adjacencyMatrix[i] = 0;
+        }
+    }
+    return (PyObject*)self;
+}
+
+static int
+Custom_init(AdjacencyMatrix* self, PyObject* args, PyObject* kwds)
+{
+    static char* kwlist[] = { "text", NULL };    
+
+    char* bytes = "?";    
+    
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "|s", kwlist, &bytes)) 
+        return -1;            
+    int verticesNumber = bytes[0]-63;    
+
+    int k = 0,i = 1;
+    int c = 0;
+    for (int v = 0; v < verticesNumber; v++) {
+        self->graph_vertices |= 1 << v;
+    }        
+    
+    for (int v = 1; v < verticesNumber; v++) {        
+        for (int u = 0; u < v; u++) {
+            if (k == 0) {
+                c = bytes[i] - 63;                
+                i++;
+                k = 6;
+            }
+            k -= 1;
+            if ((c & (1 << k)) != 0) {                                
+                self->graph_adjacencyMatrix[v] |= 1 << u;                                   
+            }
+        }
+    }    
+    return 0;
+}
+
+static PyObject*
+WheelInit(AdjacencyMatrix* self,
+    PyTypeObject* type,
+    PyObject* const* args,
+    Py_ssize_t nargs,
+    PyObject* kwds)
+{
+    printf("DD");
+    //AdjacencyMatrix*  self = (AdjacencyMatrix*)type->tp_alloc(type, 0);
+    
+    static char* kwlist[] = { "number_of_vertices", NULL };
+    int number_of_vertices = 0;
+    printf("AA\n");
+    //printf("%d\n",_unused_ignored);
+    
+    printf("%s\n", PyBytes_AS_STRING(PyUnicode_AsEncodedString(PyObject_Repr(kwds), "utf-8", "~E~")));
+    printf("D\n");
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "i", kwlist, &number_of_vertices)) {
+        printf("AAAA");
+        Py_RETURN_NONE;
+    }
+    printf("CC");
+    for (int i = 0; i < number_of_vertices; i++) {
+        self->graph_vertices |= 1 << i;
+    }
+    for (int j = 0; j < number_of_vertices; j++) {
+        self->graph_adjacencyMatrix[number_of_vertices - 1] |= 1 << j; //to center
+        AddEdge(self, j, (j + 1) % number_of_vertices);
+    }
+    printf("BB");
+    return self;
+}
+
+static PyMemberDef Custom_members[] = {
+    {"graph_vertices", T_INT, offsetof(AdjacencyMatrix, graph_vertices), 0,"custom aa"},
+    {"graph_adjacencyMatrix", T_OBJECT_EX, offsetof(AdjacencyMatrix, graph_adjacencyMatrix), 0,"custom matrix"},
+    
+    {NULL}  /* Sentinel */
+};
+
+#pragma region METHODS
+
+static PyObject*
+print_matrix(AdjacencyMatrix* self, PyObject* Py_UNUSED(ignored))
+{    
+
+    
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            if ((self->graph_adjacencyMatrix[i] & (1 << j)) != 0) {
+                printf("1");                
+            }
+            else {
+                printf("0");
+            }            
+        }
+        printf("\n");
+    }
+    Py_RETURN_NONE;
+}
+
+
+static PyObject*
+number_of_vertices(AdjacencyMatrix* self, PyObject* Py_UNUSED(ignored))
+{
+    Py_ssize_t count = 0;
+    for (int i = 0; i < 15; i++) {
+        if ((self->graph_vertices & (1 << i)) != 0) {
+            count++;
+        }
+    }
+    return PyLong_FromSsize_t(count);
+}
+
+static PyObject*
+vertices_get(AdjacencyMatrix* self, PyObject* Py_UNUSED(ignored))
+{
+    PyObject* list = PySet_New(0);    
+    for (int i = 0; i < 16; i++) {
+        if ((self->graph_vertices & (1 << i)) != 0) {
+            PySet_Add(list, PyLong_FromSsize_t(i));
+        }
+    }        
+    return list;
+}
+
+
+static PyObject*
+vertex_degree(AdjacencyMatrix* self, PyObject* index)
+{    
+    Py_ssize_t degree = 0;
+    int ind = PyLong_AsLong(index);
+
+    for (int j = 0; j < ind; j++) {
+        if ((self->graph_adjacencyMatrix[ind] & (1 << j)) != 0) {
+            degree++;
+        }
+    }
+    for (int i = ind+1; i < 16; i++) {
+        if ((self->graph_adjacencyMatrix[i] & (1 << ind)) != 0) {
+            degree++;
+        }
+    }
+    return PyLong_FromSsize_t(degree);
+}
+
+static PyObject*
+vertex_neighbors(AdjacencyMatrix* self, PyObject* index)
+{
+    PyObject* list = PySet_New(0);
+    int ind = PyLong_AsLong(index);
+    for (int j = 0; j < ind; j++) {
+        if ((self->graph_adjacencyMatrix[ind] & (1 << j)) != 0) {
+            PySet_Add(list, PyLong_FromSsize_t(j));
+        }
+    }
+    for (int i = ind + 1; i < 16; i++) {
+        if ((self->graph_adjacencyMatrix[i] & (1 << ind)) != 0) {
+            PySet_Add(list, PyLong_FromSsize_t(i));
+        }
+    }
+    return list;
+}
+
+static PyObject*
+add_vertex(AdjacencyMatrix* self, PyObject* index)
+{
+    int ind = PyLong_AsLong(index);    
+    self->graph_vertices |= (1 << ind);    
+    Py_RETURN_NONE;
+}
+
+static PyObject*
+delete_vertex(AdjacencyMatrix* self, PyObject* index)
+{
+    int ind = PyLong_AsLong(index);
+    self->graph_vertices &= ~(1 << ind);
+    self->graph_adjacencyMatrix[ind] = 0;    
+    for (int i = ind + 1; i < 16; i++) {
+        if ((self->graph_adjacencyMatrix[i] & (1 << ind)) != 0) {
+            self->graph_adjacencyMatrix[i] &= ~(1 << ind);
+        }
+    }
+    Py_RETURN_NONE;
+}
+
+static PyObject*
+number_of_edges(AdjacencyMatrix* self, PyObject* Py_UNUSED(ignored))
+{
+    int count = 0;
+    for (int i = 1; i < 15; i++) {        
+        for (int j = 0; j < i; j++) {                        
+            if (((self->graph_adjacencyMatrix[i]) & ( 1 << j)) != 0) {                
+                count++;
+            }
+        }
+    }
+    return PyLong_FromSsize_t(count);
+}
+
+static PyObject*
+edges(AdjacencyMatrix* self, PyObject* Py_UNUSED(ignored))
+{
+    PyObject* list = PySet_New(NULL);
+    for (int i = 1; i < 15; i++) {
+        for (int j = 0; j < i; j++) {
+            if (((self->graph_adjacencyMatrix[i]) & (1 << j)) != 0) {
+                PyObject* tuple = PyTuple_New(2);                
+                PyTuple_SetItem(tuple, 0, PyLong_FromLong(j));                
+                PyTuple_SetItem(tuple, 1, PyLong_FromLong(i));                
+                PySet_Add(list, tuple);                
+            }
+        }
+    }
+    return list;
+}
+
+static PyObject*
+is_edge(AdjacencyMatrix* self, PyObject* args, PyObject* kwargs)
+{
+    static char* kwlist[] = { "i", "j", NULL };    
+    int i,j;    
+ 
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii", kwlist, &i, &j)) {        
+        Py_RETURN_NONE;
+    }            
+
+    if (i > j) {
+        int temp = i;
+        i = j;
+        j = temp;
+    }
+    return  (((self->graph_adjacencyMatrix[j]) & (1 << i)) != 0)?
+            PyBool_FromLong(1):
+            PyBool_FromLong(0);
+}
+
+
+static PyObject*
+add_edge(AdjacencyMatrix* self, PyObject* args, PyObject* kwargs)
+{
+    
+    static char* kwlist[] = { "i", "j", NULL };
+    int i, j;
+        
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii", kwlist, &i, &j)) {        
+        Py_RETURN_NONE;
+    }    
+    AddEdge(self, i, j);
+    Py_RETURN_NONE;
+}
+
+static PyObject*
+delete_edge(AdjacencyMatrix* self, PyObject* args, PyObject* kwargs)
+{
+    static char* kwlist[] = { "i", "j", NULL };
+    int i, j;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ii", kwlist, &i, &j)) {
+        Py_RETURN_NONE;
+    }
+        
+
+    self->graph_adjacencyMatrix[i] &= ~( 1 << j);    
+    self->graph_adjacencyMatrix[j] &= ~( 1 << i);
+    
+    Py_RETURN_NONE;
+}
+
+
+
+static PyMethodDef Custom_methods[] = {
+    {"number_of_vertices", (PyCFunction)number_of_vertices, METH_NOARGS,""},
+    {"vertices", (PyCFunction)vertices_get, METH_NOARGS,""},
+    {"vertex_degree", (PyCFunction)vertex_degree, METH_O,""},
+    {"vertex_neighbors", (PyCFunction)vertex_neighbors, METH_O,""},
+    {"add_vertex", (PyCFunction)add_vertex, METH_O,""},
+    {"delete_vertex", (PyCFunction)delete_vertex, METH_O,""},
+    {"number_of_edges", (PyCFunction)number_of_edges, METH_NOARGS,""},
+    {"edges", (PyCFunction)edges, METH_NOARGS,""},
+    {"is_edge", (PyCFunction)is_edge, METH_VARARGS | METH_KEYWORDS,""},
+    {"add_edge", (PyCFunction)add_edge, METH_VARARGS | METH_KEYWORDS,""},
+    {"delete_edge", (PyCFunction)delete_edge, METH_VARARGS | METH_KEYWORDS,""},
+    {"print_matrix", (PyCFunction)print_matrix, METH_NOARGS,""},
+    {"create_wheel", (PyCMethod)WheelInit, METH_STATIC | METH_FASTCALL | METH_KEYWORDS  ,""},
+    {NULL}  /* Sentinel */
+};
+#pragma endregion METHODS
+
+static PyTypeObject AdjacencyMatrixType = {
+    .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "AdjacencyMatrixType",
+    .tp_doc = PyDoc_STR("Custom objects"),
+    .tp_basicsize = sizeof(AdjacencyMatrix),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_new = Custom_new,
+    .tp_init = (initproc)Custom_init,
+    .tp_dealloc = (destructor)Custom_dealloc,
+    .tp_members = Custom_members,
+    .tp_methods = Custom_methods,    
+};
+
+static PyModuleDef custommodule = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "custom2",
+    .m_doc = "Example module that creates an extension type.",
+    .m_size = -1,
+};
+
+PyMODINIT_FUNC
+PyInit_simple_graphs(void)
+{
+    PyObject* m;
+    if (PyType_Ready(&AdjacencyMatrixType) < 0)
+        return NULL;
+
+    m = PyModule_Create(&custommodule);
+    if (m == NULL)
+        return NULL;
+
+    if (PyModule_AddObjectRef(m, "AdjacencyMatrix", (PyObject*)&AdjacencyMatrixType) < 0) {
+        Py_DECREF(m);
+        return NULL;
+    }
+    
+    return m;
+}
--- python/main.py
+++ python/main.py
@@ -1,17 +1,34 @@
 import pybind11
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from  simple_graphs import *
+from graphs import *
+graf  = AdjacencyMatrix("DQc")
+a=1
+
+print("number_of_vertices: " + str(graf.number_of_vertices() == 5))
+print("vertices: " + str(graf.vertices() == {0,1,2,3,4}))
+print("vertex_degree: " + str(graf.vertex_degree(4) == 2))
+print("vertex_degree: " + str(graf.vertex_degree(5) == 0))
+print("vertex_neighbors: " + str(graf.vertex_neighbors(4) == {0,3}))
+graf.add_vertex(15)
+print("add_vertex: " + (str(True) if (graf.vertices() == {0,1,2,3,4,15}) else ('returned: ' + str(graf.vertices()))))
+graf.delete_vertex(15)
+print("vertices: " + str(graf.vertices() == {0,1,2,3,4}))
+
+print("number_of_edges: " + str(graf.number_of_edges() == 4) + ", counted: " + str(graf.number_of_edges()))
+print("edges: " + str(graf.edges() == {(1,3),(3,4),(0,4),(0,2)}))
+print("is_edge: " + str(graf.is_edge(0,2) == True))
+print("is_edge: " + str(graf.is_edge(0,1) == False))
+graf.delete_edge(0,2)
+print("delete_edge: " + str(graf.is_edge(0,2) == False))
+graf.add_edge(0,3)
+print("add_edge: " + str(graf.is_edge(0,3) == True))
+
+graf2 = Graph("DQc")
+graf2.add_vertex(2)
+#print(graf2.edges())
+
+graf = AdjacencyMatrix.create_wheel(5)
+a=2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
